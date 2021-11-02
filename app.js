b8c079f80ab568afc085544e6b3c6bc223acb596
kate <katecr17@gmail.com>
2021-11-02 16:40:14 -0500

    [add] Path connection to home.html and its css

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// Declarando los modulos externos de express
 const express = require('express');
 const app = express();
+const path = require('path');
+
+//asignando la carpeta public como recurso estatico
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
 
 const routesMain = require('./routers/main.js');
 
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,15 @@
+// Declarando los modulos externos de express
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//asignando la carpeta public como recurso estatico
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
 let mainController = {
   index: function(req,res){
-    res.send("Bienvenido al Inicio del portafolio");
+    res.sendFile(path.resolve(__dirname, '../views/home.html'));
   },
 }
 
